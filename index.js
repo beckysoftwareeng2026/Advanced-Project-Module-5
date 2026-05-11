3cfc37d12d1dc71dd4e61815eac6f98d8b948791
Jennifer Cobb <jennifercobb829@gmail.com>
2026-05-11 19:56:10 -0400

    Updating

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function moveBackground(event) {
   for (let i = 0; i < shapes.length; ++i) {
     const isOdd = i % 2 !== 0;
     const boolInt = isOdd ? -1 : 1;
+    // Added rotate after tutorial
     shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
   }
 }
@@ -31,10 +32,10 @@ function contact(event) {
   loading.classList += " modal__overlay--visible";
   emailjs
     .sendForm(
-      "service_80ih0if",
-      "template_d9refyl",
+      "service_g0yjped",
+      "template_8njakj3",
       event.target,
-      "user_K1PoFs8pB2YVWStDxrUls"
+      "f6i8YNXr8vT28SPdI"
     )
     .then(() => {
       loading.classList.remove("modal__overlay--visible");
@@ -43,7 +44,7 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        "The email service is temporarily unavailable. Please contact me directly on jennifercobb829@gmail.com"
       );
     });
 }
